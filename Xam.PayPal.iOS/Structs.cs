2619bc5682589d8710f106738d12bb7b087b5227
Marco Milani <emme.milani@hotmail.it>
2017-01-04 00:20:11 +0100

    START

---
+++ Xam.PayPal.iOS/Structs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace Xam.PayPal.iOS
+{
+	[Native]
+	public enum CardIOCreditCardType : long
+	{
+		Unrecognized = 0,
+		Ambiguous = 1,
+		Amex = 51,
+		Jcb = 74,
+		Visa = 52,
+		Mastercard = 53,
+		Discover = 54
+	}
+
+	[Native]
+	public enum CardIODetectionMode : long
+	{
+		CardImageAndNumber = 0,
+		CardImageOnly,
+		Automatic
+	}
+
+	[Native]
+	public enum PayPalShippingAddressOption : long
+	{
+		None = 0,
+		Provided = 1,
+		PayPal = 2,
+		Both = 3
+	}
+
+	[Native]
+	public enum PayPalPaymentIntent : long
+	{
+		Sale = 0,
+		Authorize = 1,
+		Order = 2
+	}
+
+	[Native]
+	public enum PayPalPaymentViewControllerState : long
+	{
+		Unsent = 0,
+		InProgress = 1
+	}
+}
